8b5e2903a6234f85221fd6bf063a5a6b150b6936
Krzysztof Kabala <krzykab@gmail.com>
2018-11-26 08:57:30 +0100

    Fix NewNode model

--- src/GinPlatform.NET SDK/Models/Node/NewNode.cs
+++ src/GinPlatform.NET SDK/Models/Node/NewNode.cs
@@ -2,20 +2,20 @@
 
 namespace GinPlatform.NET_SDK.Models.Node
 {
-    public class NewNode
-    {
-        public NewNode(int dedicated, string blockchain, int collateral, string txid, Dictionary<string, string> meta)
-        {
-            Dedicated = dedicated;
-            Blockchain = blockchain;
-            Collateral = collateral;
-            Txid = txid;
-            Meta = meta;
-        }
-        public int Dedicated { get; }
-        public string Blockchain { get; }
-        public int    Collateral { get; }
-        public string Txid { get; }
-        Dictionary<string, string> Meta { get;}
-    }
+	public class NewNode
+	{
+		public NewNode(int dedicated, string blockchain, int collateral, string txid, Dictionary<string, string> meta = null)
+		{
+			Dedicated = dedicated;
+			Blockchain = blockchain;
+			Collateral = collateral;
+			Txid = txid;
+			Meta = meta ?? new Dictionary<string, string>();
+		}
+		public int Dedicated { get; }
+		public string Blockchain { get; }
+		public int Collateral { get; }
+		public string Txid { get; }
+		public Dictionary<string, string> Meta { get; }
+	}
 }
