9ddbda38b5d981f9dcab28722acfedf75cf0d48b
Krzysztof Kabala <krzykab@gmail.com>
2018-12-05 15:51:14 +0100

    Add financials data to the node model

---
+++ src/GinPlatform.NET SDK/Models/Node/Financials.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace GinPlatform.NET_SDK.Models.Node
+{
+    public class Financials
+    {
+        [JsonProperty("last_reward_at")]
+        public DateTime LastRewardAt { get; set; }
+
+        [JsonProperty("rewards")]
+        public int Rewards { get; set; }
+
+        [JsonProperty("rewards_today")]
+        public int RewardsToday { get; set; }
+
+        [JsonProperty("rewards_month")]
+        public int RewardsMonth { get; set; }
+
+        [JsonProperty("exits")]
+        public double Exits { get; set; }
+
+        [JsonProperty("exits_btc_val")]
+        public double ExitsBtcVal { get; set; }
+    }
+}
